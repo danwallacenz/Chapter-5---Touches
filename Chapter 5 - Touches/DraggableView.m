26fb43f4324d6999de6b05bfb1020195a08d3f90
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-07-12 11:01:53 +1200

    add position labels

--- Chapter 5 - Touches/DraggableView.m
+++ Chapter 5 - Touches/DraggableView.m
@@ -8,22 +8,52 @@
 
 #import "DraggableView.h"
 
+@interface DraggableView ()
+
+@property (strong, nonatomic) UILabel *positionInSelfLabel;
+@property (strong, nonatomic) UILabel *positionInSuperviewLabel;
+
+@end
+
 @implementation DraggableView
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self setup];
     }
     return self;
 }
+
+- (void) setup
+{
+    self.positionInSelfLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width, 20.0f)];
+    [self addSubview:self.positionInSelfLabel];
+    
+    self.positionInSuperviewLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20.0f, self.bounds.size.width, 20.0f)];
+    [self addSubview:self.positionInSuperviewLabel];
+    
+    self.positionInSelfLabel.text =  NSStringFromCGPoint(CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds)));
+    self.positionInSuperviewLabel.text =   NSStringFromCGPoint(self.center); 
+}
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"touchesBegan");
     [self logTouches:touches];
     [self logEvent:event];
-    
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
@@ -77,31 +107,34 @@
         NSString *touchPhaseDescription = @"unknown phase";
         switch (phase) {
             case UITouchPhaseBegan:
-                touchPhaseDescription = @"UITouchPhaseBegan";
+                touchPhaseDescription = @"Began";
                 break;
             case UITouchPhaseMoved:
-                touchPhaseDescription = @"UITouchPhaseMoved";
+                touchPhaseDescription = @"Moved";
                 break;
             case UITouchPhaseStationary:
-                touchPhaseDescription = @"UITouchPhaseStationary";
+                touchPhaseDescription = @"Stationary";
                 break;
             case UITouchPhaseEnded:
-                touchPhaseDescription = @"UITouchPhaseEnded";
+                touchPhaseDescription = @"Ended";
                 break;
             case UITouchPhaseCancelled:
-                touchPhaseDescription = @"UITouchPhaseCancelled";
+                touchPhaseDescription = @"Cancelled";
                 break;
             default:
                 break;
         }
         
         NSLog(@"\n\ntouch - phase: %@ \nlocation in draggable view: %@ \nsuperview location: %@ \ntimestamp:%f \ntapCount: %lu \ntapped view: %@\n\n", touchPhaseDescription, NSStringFromCGPoint(location), NSStringFromCGPoint(superviewLocation), timestamp, (unsigned long)tapCount, [tappedView class]);
+        
+        self.positionInSelfLabel.text =  NSStringFromCGPoint(location);
+        self.positionInSuperviewLabel.text =  NSStringFromCGPoint(superviewLocation);
     }
 }
 
 - (void) logEvent: (UIEvent *)event
 {
-//    NSLog(@"event: %@", event);
+    NSLog(@"\nevent - count:%lu\n", (unsigned long)[event touchesForView:self].count);
 }
 
 @end
