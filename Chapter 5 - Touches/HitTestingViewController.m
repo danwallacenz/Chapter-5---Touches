e1fe3dd856a53432e8f9c704549557b6ee4be0ae
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-07-15 17:10:07 +1200

    trying to get layer hit testing working

--- Chapter 5 - Touches/HitTestingViewController.m
+++ Chapter 5 - Touches/HitTestingViewController.m
@@ -14,6 +14,10 @@
 @property (weak, nonatomic) IBOutlet UIView *hitTestView;
 @property (weak, nonatomic) IBOutlet UIView *tapOutsideBoundsView;
 
+@property (weak, nonatomic) IBOutlet UIView *layerHitTestView;
+@property (weak, nonatomic)CALayer *redRectLayer;
+@property (weak, nonatomic)CALayer *greenRectLayer;
+
 @end
 
 @implementation HitTestingViewController
@@ -28,6 +32,47 @@
     if([tappedView isKindOfClass:[UIImageView class]]){
         [self swellBriefly: tappedView];
     }
+}
+
+- (IBAction)tapLayers:(UITapGestureRecognizer *)sender
+{
+    NSLog(@"tapLayers:");
+    
+    CGPoint tapPoint = [sender locationOfTouch:0 inView: sender.view];
+    NSLog(@"tap at %@ in view.", NSStringFromCGPoint(tapPoint));
+    
+    NSLog(@"layer frame = %@", NSStringFromCGRect(sender.view.layer.frame));
+    
+    CALayer *result = [sender.view.layer hitTest:tapPoint];
+    
+    NSLog(@"result = %@",result);
+    
+    if (result == self.redRectLayer || result == self.greenRectLayer) {
+        NSString *layerDesc = @"Missed";
+        if(result == self.redRectLayer){
+            layerDesc = @"red";
+        }
+        if(result == self.greenRectLayer){
+            layerDesc = @"green";
+        }
+        NSLog(@"a hit at %@ on %@", NSStringFromCGPoint(tapPoint), layerDesc );
+//        [self colorBriefly:result withColor:[UIColor brownColor]];
+    }
+//    }
+    
+//    for (CALayer *subLayer in [sender.view.layer.sublayers reverseObjectEnumerator]) {
+//        NSLog(@"subLayer frame = %@", NSStringFromCGRect(subLayer.frame));
+//
+//        CGPoint pt = [subLayer convertPoint:tapPoint fromLayer:sender.view.layer];
+//        
+//        NSLog(@"tap at %@", NSStringFromCGPoint(pt));
+//        CALayer *result = [subLayer hitTest:pt];
+//        if (result == self.redRectLayer || result == self.greenRectLayer ) {
+//            NSLog(@"a hit at %@ on %@", NSStringFromCGPoint(pt), result == self.redRectLayer?@"red":@"missed red" );
+//            [self colorBriefly:result withColor:[UIColor brownColor]];
+////            break;
+//        }
+//    }
 }
 
 -(void)swellBriefly:(UIView *)view
@@ -49,12 +94,44 @@
                      }];
 }
 
+-(void)colorBriefly:(CALayer *)layer withColor: (UIColor *)color
+{
+    CGColorRef originalColor = layer.backgroundColor;
+    [UIView animateWithDuration:0.25
+                          delay: 0
+                        options: 0//UIViewAnimationOptionAutoreverse
+                     animations:^{
+                         layer.backgroundColor = color.CGColor;
+                     }completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.25
+                                          animations:^{
+                                              layer.backgroundColor = originalColor;
+                                          }];
+                     }];
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.tapOutsideBoundsView.clipsToBounds = NO;
+    
+    
+    CALayer *redRectLayer = [[CALayer alloc] init];
+    redRectLayer.bounds = CGRectMake(0, 0, 100, 100);
+    redRectLayer.position = CGPointMake(100, 100);
+    redRectLayer.backgroundColor = [UIColor redColor].CGColor;
+    [self.layerHitTestView.layer addSublayer:redRectLayer];
+    self.redRectLayer = redRectLayer;
+    
+    CALayer *greenRectLayer = [[CALayer alloc] init];
+    greenRectLayer.bounds = CGRectMake(0, 0, 100, 100);
+    greenRectLayer.position = CGPointMake(300, 100);
+    greenRectLayer.backgroundColor = [UIColor greenColor].CGColor;
+    [self.layerHitTestView.layer addSublayer:greenRectLayer];
+    self.greenRectLayer = greenRectLayer;
+    
 }
 
 
